93ec318a11d8c6d0cb35e4690c0a902e648f96d1
wangyinan <wyn153409018@outlook.com>
2017-01-14 13:19:19 +0800

    fix: not lint *.json

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-
 const fs = require('fs');
 const path = require('path');
 const dotenv = require('dotenv');
-
 const envConfigPath = path.join(__dirname, 'env.config');
 if (fs.existsSync(envConfigPath)) {
   dotenv.config({
